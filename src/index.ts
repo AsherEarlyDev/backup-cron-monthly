a31b936454e495d62f90a4570db312c53ae9bed4
Jeremy Cross <remycross1019@gmail.com>
2023-03-01 16:59:50 -0500

    Added logtail stuff

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,19 @@
 import { CronJob } from "cron";
-
 import { backup } from "./backup";
 import { env } from "./env";
+const { Logtail } = require("@logtail/node");
+const logtail = new Logtail("ZpUA5eyVBQXQwy8kLtno83tT>");
 
 const job = new CronJob(env.BACKUP_CRON_SCHEDULE, async () => {
   try {
     await backup();
   } catch (error) {
-    console.error("Error while running backup: ", error)
+    console.error("Error while running monthly backup: ", error)
+    logtail.error("Error while running monthly backup", error);
   }
 });
 
 job.start();
 
 console.log("Backup cron scheduled...")
+logtail.info("Backup cron scheduled...");
